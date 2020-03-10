322ea65718b7391cc74b38c53c06c92e25fefdec
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2020-03-10 19:59:55 +0100

    Update microsoft2020_3.cpp

--- microsoft2020_3.cpp
+++ microsoft2020_3.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 int main(){
     int n;
-    int min, max, i_min=0, curr_max=0, count=1; //min and max are both initialized in "if" at line 17
+    int min, max, i_min=0, curr_max=0, count=1; //min is initialized in "if" at line 15
     int i; 
     vector<int> array;
+    stack<int> every_max;
     cin >> n; 
 
     for(i=0; i < n; i++){ //here we will collect the input and memorize the values of min, max and i_min
@@ -30,23 +32,29 @@ int main(){
         if(curr_max < array[i])
             curr_max = array[i];
     }
-    int slice_size = i_min;
+    bool slice_size; //this boolean represents whether or not the slice cointains more than 1 element
+    i_min >= 1 ? slice_size = true : slice_size = false; 
     
-    //the value of i is i_min+1 (i = i_min+1), with that in mind we can do the following:
+    //the value of i is i_min (i=i_min), with that in mind we can do the following:
     if(curr_max != max)
         while(i < n){
             if(array[i] == max){
-                if(slice_size != 1)
+                if(slice_size)
                     count++;
                 break;
             }
             else if(array[i] > curr_max){
                 count++;
+                every_max.push(curr_max);
                 curr_max = array[i];
-                slice_size=1;
+                slice_size=false;
             }
             else{
-                slice_size++;
+                while(!every_max.empty() && every_max.top() > array[i]){
+                    count--;
+                    every_max.pop();
+                }
+                slice_size=true;
             }
             i++;
         }
